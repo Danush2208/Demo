9b43246ca0ec6d37dde62f35de4edc58fb2a188f
Danush2208 <danushvenkat25@gmail.com>
2021-06-26 20:26:18 +0530

    new change

--- hello.c
+++ hello.c
@@ -1,3 +1,13 @@
+/**
+ * @file hello.c
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-06-26
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #include <stdio.h>
 int main(){
     printf("Hello");
